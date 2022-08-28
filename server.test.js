264ba54e38160dad440435b3a055421897dac266
Dane David <dndavid102@gmail.com>
2022-08-28 16:31:52 +0200

    Add test for bad requests

--- server.test.js
+++ server.test.js
@@ -4,13 +4,27 @@ const server = require('./server.js');
 
 const request = supertest(server);
 
+const badRequestTestCases = [
+  { url: '/geojson-data?bbox=-89,51.28' },
+  { url: '/geojson-data' },
+];
+
 describe('GeoJSON Endpoint', () => {
-  it('GET /geojson-data should return valid GeoJSON', async () => {
+  it('should return valid GeoJSON', async () => {
     const res = await request.get('/geojson-data?bbox=-89,51.28,-88.9,51.29');
 
     expect(res.headers['content-type']).toMatch(/json/);
     expect(res.status).toEqual(200);
     expect(res.body).toBeDefined();
     expect(gjv.valid(res.body)).toBe(true);
+  });
+
+  it('should return 400 for invalid query params', async () => {
+    for (const testCase of badRequestTestCases) {
+      const res = await request.get(testCase.url);
+
+      expect(res.headers['content-type']).toMatch(/json/);
+      expect(res.status).toEqual(400);
+    }
   });
 });
